7141eb45951c7f1ebcfbc8a198a16545e638628a
ht68rohit <46751484+ht68rohit@users.noreply.github.com>
2019-03-05 22:39:24 +0530

    Update card_test.go

--- CardOperation/card_test.go
+++ CardOperation/card_test.go
@@ -17,21 +17,22 @@ type Card struct {
 }
 
 var _ = Describe("Create Card operations", func() {
-
-	card := Card{}
+	os.Setenv("SECRET_KEY", "sk_test_Ma5sDiK8tRrvg1eHVew4SYEN")
+	var param stripe.CardParams
 	reqbody := new(bytes.Buffer)
-	json.NewEncoder(reqbody).Encode(card)
+	json.NewEncoder(reqbody).Encode(param)
 
 	req, err := http.NewRequest("POST", "/createcard", reqbody)
 	if err != nil {
 	}
 	recorder := httptest.NewRecorder()
-
+	handler := http.HandlerFunc(CreateCard)
+	handler.ServeHTTP(recorder, req)
+	
 	Describe("create card", func() {
 		Context("CreateCard", func() {
-			It("Should result http.StatusCreated", func() {
-				CreateCard(recorder, req)
-				Expect(result.GetResultCreated()).To(Equal(http.StatusCreated))
+			It("Should result http.StatusBadRequest", func() {
+				Expect(recorder.Code).To(Equal(http.StatusBadRequest))
 			})
 		})
 	})
